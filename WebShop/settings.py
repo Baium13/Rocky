620ab7680cdfd38ce7ed76d2c4f9a7fc74f635fc
Artem Fedorov <artem@feezback.com>
2023-12-30 13:08:56 +0100

    Implement basic page

--- WebShop/settings.py
+++ WebShop/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'WebShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
